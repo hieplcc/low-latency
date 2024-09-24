2c85589ddcf09ca0899452873e9f9c904f411dcf
a1den <lechihiep@icloud.com>
2024-09-24 18:49:09 +0700

    update

--- techniques/vectorize/vectorize.cpp
+++ techniques/vectorize/vectorize.cpp
@@ -1,35 +1,42 @@
-#include <benchmark/benchmark.h>
 #include <cstring>
 #include <iostream>
+#include <chrono>
 
 //Compiler can NOT vectorized the arrays
 // since the current iteration depends on the result of previous one.
-static void Unvectorizable(benchmark::State& state) {
-    for (auto _ : state) {
-        int a[1000], b[1000], c[1000];
+int a[100000], b[100000], c[100000];
 
-        for (int i = 0; i <= 998; ++i) {
-            a[i]     += b[i];
-            b[i + 1] += c[i];
-        }
+void unvectorizable() {
+    auto start = std::chrono::high_resolution_clock::now();
+
+    for (int i = 0; i <= 99998; ++i) {
+        a[i]     += b[i];
+        b[i + 1] += c[i];
     }
+
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+    std::cout << "unvectorizable takes: " << duration << " (us)" << std::endl;
 }
-BENCHMARK(Unvectorizable);
 
 //Compiler can vectorize the arrays and does SIMD operations
-static void Vectorizable(benchmark::State& state) {
-    for (auto _ : state) {
-        int a[1000], b[1000], c[1000];
-        a[1] += b[1]; 
-        for (int i = 1; i <= 998; ++i) {
-            b[i + 1] += c[i];
-            a[i + 1] += b[i + 1];
-        }
+void vectorizable() {
+    auto start = std::chrono::high_resolution_clock::now();
+
+    a[1] += b[1]; 
+    for (int i = 1; i <= 99998; ++i) {
+        b[i + 1] += c[i];
+        a[i + 1] += b[i + 1];
     }
+    
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+    std::cout << "vectorizable takes: " << duration << " (us)" << std::endl;
 }
-BENCHMARK(Vectorizable);
 
-// https://quick-bench.com
-
-BENCHMARK_MAIN();
-
+//Need to compile with -O3 so compiler can optimize and vectorize the input.
+int main() {
+    unvectorizable();
+    vectorizable();
+    return 1;
+}
